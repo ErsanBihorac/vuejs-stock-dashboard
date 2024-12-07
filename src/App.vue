994f8854a4f3526a29065412d61e8ac13be71a35
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-12-07 23:36:34 +0100

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="app">
-
-  </div>
+    <BaseCard>
+      <h1>Überschrift</h1>
+      <p>paragraph</p>
+    </BaseCard>
 </template>
 
 <script>
+import BaseCard from './components/BaseCard.vue';
 
 export default {
   name: 'App',
   components: {
+    BaseCard
   }
 }
 </script>
@@ -19,8 +22,10 @@ body {
 }
 
 #app {
-  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
   width: 100vw;
   min-height: 100vh;
+  padding: 100px;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  box-sizing: border-box;
 }
 </style>
